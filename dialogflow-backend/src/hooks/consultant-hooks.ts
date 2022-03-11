9b84966890ec514adb8985e440340989eed7dcc9
Yentl Projects <yentlprojects@gmail.com>
2022-03-11 14:33:59 +0100

    Problem Report I

--- dialogflow-backend/src/hooks/consultant-hooks.ts
+++ dialogflow-backend/src/hooks/consultant-hooks.ts
@@ -1,10 +1,11 @@
 import { WebhookClient } from 'dialogflow-fulfillment';
 
 
-function displayNameHandler(agent: WebhookClient): void {
+function problemReportHandler(agent: WebhookClient): void {
+    console.log('Reporting problem');
     agent.add('Yes!');
 }
 
 export default function(intentMap: Map<string, Function>) {
-    // intentMap.set('intent.displayName', displayNameHandler);
+    intentMap.set('consultant.ProblemReport', problemReportHandler);
 }
